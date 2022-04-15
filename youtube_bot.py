94c6b9a60b8dc91d432866d2dc4e892f6f17752d
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-04-15 13:27:49 +0545

    Update youtube_bot.py

--- youtube_bot.py
+++ youtube_bot.py
@@ -1,63 +1,60 @@
-from urllib import response
-from aiohttp import request
+# rank the channels
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
 import os
+from math import log, floor
 import json
-import requests
+
 
 load_dotenv()
 
+
 youtube = build('youtube','v3', developerKey = os.getenv('api_key'))
 
-initial_request = youtube.channels().list(
-            part = 'contentDetails, statistics',
-            forUsername = 'sentdex'
-)
 
-initial_respons= initial_request.execute()
+# function to return the name, average view on videos and 
+# subscriber count according to channel_name as in 'file.txt'
+def get_channel_avg_views_and_subs():
+	final_list = []
+	with open('input.txt') as f:
+		for line in f:
+			for channel_name in line.split():
+				initial_request = youtube.channels().list(
+							part = 'contentDetails, statistics',
+							forUsername = channel_name
+				)
+				initial_respons= initial_request.execute()
+				total_videos = initial_respons['items'][0]['statistics']['videoCount']
+				total_subs = initial_respons['items'][0]['statistics']['subscriberCount']
+				total_view_count = initial_respons['items'][0]['statistics']['viewCount']
+				avg_views= int(total_view_count)/int(total_videos) 		# average views in all channels
+				info_dict = {'name':channel_name,'avg_view':str(get_avg(avg_views)),'subcribers_count':str(total_subs)}
+				final_list.append(info_dict)
 
-channel_id = initial_respons['items'][0]['id']
-total_videos = initial_respons['items'][0]['statistics']['videoCount']
-print(channel_id,total_videos)
-## display the most popular videos, titles and view count
-
-video_ids = youtube.activities().list(
-            part = 'contentDetails',
-           channelId= channel_id
-)
-
-video_ids_respons= video_ids.execute()
-# print(video_ids_respons)
-with open('file.json', "w+") as f:
-	json.dump(video_ids_respons, f, indent = 2)
-
-# # print(video_ids)
-# video_request = youtube.videos().list(
-#         part="snippet,contentDetails,statistics",
-#         id="Ks-_Mh1QhMc"
-# )
-# video_respons= video_request.execute()
-# print(video_respons)
+	return final_list
 
 
-# # most popular videos according to a regiom
-        
-# video_popular_request = youtube.videos().list(
-#        part="contentDetails,statistics",
-#         chart="mostPopular",
-#         regionCode="US"
-# )
-# video_popular_respons= video_popular_request.execute()
+# function to give tags for numbers like thousand, million,etc
+def get_avg(avg_view):
+	units = ['', 'K', 'M', 'G', 'T', 'P']
+	k = 1000.0
+	magnitude = int(floor(log(avg_view, k)))
+	return '%.2f%s' % (avg_view / k**magnitude, units[magnitude])
+  
+
+# saves the final_result to file.json
+def save_to_file(final_result):
+	with open('file.json', "w+") as f:
+		json.dump(result, f, indent = 2)
 
 
-# getting commments
-request = youtube.comments().list(
-        part="snippet",
-        parentId="UgzDE2tasfmrYLyNkGt4AaABAg"
-    )
-comment_response = request.execute()
+# opens file.json, reads it and parse according to use
+def read_data():
+	with open('file.json', "r") as f:
+		data = json.load(f)
+		for i in range(0, len(data)):
+			print(data[i]['name']) # returns name of each channel
+			
 
-print(comment_response)
-with open('file.json', "w+") as f:
-	json.dump(comment_response, f, indent = 2)
+result = get_channel_avg_views_and_subs()
+save_to_file(result)
