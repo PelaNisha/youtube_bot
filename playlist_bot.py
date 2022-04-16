29072c12f0d5ffc19af5fcd3b7c905c45f07be99
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-04-16 20:23:49 +0545

    Update playlist_bot.py

--- playlist_bot.py
+++ playlist_bot.py
@@ -26,27 +26,27 @@ def get_playlist():
 			for channel_name in line.split():
 
 				# to return the channel_id of the channels
-				initial_request = youtube.channels().list(
+				channel_id_request = youtube.channels().list(
 				part = 'contentDetails, statistics',
 				forUsername = channel_name
 				)
-				initial_respons= initial_request.execute()
-				channel_id = initial_respons['items'][0]['id']				
+				channel_id_respons= channel_id_request.execute()
+				channel_id = channel_id_respons['items'][0]['id']				
 				
 				# to return the plalists of a channel using the channel id
-				final_request = youtube.playlists().list(
+				playlist_request = youtube.playlists().list(
 					part="snippet,contentDetails",
 					channelId= channel_id,
 					maxResults=5
 				)
-				final_respons= final_request.execute()
-				for item in final_respons['items']:
-					published_title = item['snippet']['title']
+				playlist_respons=playlist_request.execute()
+				for item in playlist_respons['items']:
+					playlist_published_title = item['snippet']['title']
 					playlist_date = item['snippet']['publishedAt']
 					description = item['snippet']['description']		
 					playlist_id = item['id'] # playlist id to get all the videos in a playlist
 					playlist_videos = get_video_titles_and_duration(get_videos_of_playlists(playlist_id)) # get the videos using the get_video_titles_and_duration funtion
-					info_dict = {'published_title':published_title,'playlist date':playlist_date,'description':description, 'playlist_videos':playlist_videos}
+					info_dict = {'published_title':playlist_published_title,'playlist date':playlist_date,'description':description, 'playlist_videos':playlist_videos}
 					final_list.append(info_dict)
 					
 	# sort the final_list according to the playlist_date
@@ -57,13 +57,13 @@ def get_playlist():
 # to get all the videos in a playlist using the playlist id
 def get_videos_of_playlists(playlist_id):
 	video_id_list = []
-	initial_request = youtube.playlistItems().list(
+	playlist_video_request = youtube.playlistItems().list(
 					part="contentDetails",
 					playlistId=playlist_id,
 					maxResults=5
 	)
-	initial_response = initial_request.execute()
-	for item in initial_response['items']:
+	playlist_video_response = playlist_video_request.execute()
+	for item in playlist_video_response['items']:
 		video_id_list.append(item['contentDetails']['videoId'])
 	return video_id_list  
 
@@ -73,14 +73,14 @@ def get_video_titles_and_duration(video_ids):
 	final_list = []
 
 	# make the request for videos
-	initial_request = youtube.videos().list(
+	video_request = youtube.videos().list(
 					part="contentDetails,snippet",
 					id = ','.join(video_ids)
 	)
-	initial_response = initial_request.execute()
+	video_response = video_request.execute()
 
 	# get the content video title and duration
-	for item in initial_response['items']:
+	for item in video_response['items']:
 		time_duration = item['contentDetails']['duration']
 
 		# changing the format for time 
@@ -88,9 +88,9 @@ def get_video_titles_and_duration(video_ids):
 		sec_pattern = re.compile(r'(\d+)S')
 		minutes = mins_pattern.search(time_duration)
 		seconds = sec_pattern.search(time_duration)
-		final_min = minutes.group(1) if minutes else 0
-		final_sec = seconds.group(1)  if seconds else 0
-		final_time = str(final_min)+"mins "+str(final_sec)+"secs"
+		formatted_min = minutes.group(1) if minutes else 0
+		formatted_sec = seconds.group(1)  if seconds else 0
+		final_time = str(formatted_min)+"mins "+str(formatted_sec)+"secs"
 
 		title_duration_dict = {'title':item['snippet']['title'],'duration':final_time}
 		final_list.append(title_duration_dict)
@@ -101,6 +101,7 @@ def get_video_titles_and_duration(video_ids):
 def save_to_file(final_result):
 	with open('file1.json', "w+") as f:
 		json.dump(final_result, f, indent = 2)
+
 
 x =get_playlist()
 save_to_file(x)
